d4ac478efcec26df58f428ab354d7818e91aaae7
Ayush Choudhary <73476524+BRAGA100@users.noreply.github.com>
2025-03-19 18:33:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
 const inputBox=document.getElementById("input-box");
 const listContainer=document.getElementById("list-container");
 
-function addTask(){
-    if(inputBox.value === ''){
-        alert('Please add you task');
-    }
-   else{
-    let li=document.createElement("li");
-    li.innerHTML=inputBox.value;
-    listContainer.appendChild(li);
+// function addTask(){
+//     if(inputBox.value === ''){
+//         alert('Please add you task');
+//     }
+//    else{
+//     let li=document.createElement("li");
+//     li.innerHTML=inputBox.value;
+//     listContainer.appendChild(li);
 
-    let span=document.createElement("span");
-    span.innerHTML="\u00d7";
-    li.appendChild(span);
-   }
-   inputBox.value="";
-   saveData();
+//     let span=document.createElement("span");
+//     span.innerHTML="\u00d7";
+//     li.appendChild(span);
+//     let span1=document.createElement("span");
+//     span.innerHTML="Date()";
+//     li.appendChild(span1);
+//    }
+//    inputBox.value="";
+//    saveData();
+// }
+
+// Updated addtask function:
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert('Please add your task');
+    } else {
+        let li = document.createElement("li");
+        
+        // Get current date and time
+        let now = new Date();
+        let options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true };
+        let formattedDate = now.toLocaleString('en-US', options);
+
+        // Set task with date
+        li.innerHTML = `${inputBox.value} <br><small style="color: gray;">${formattedDate}</small>`;
+        listContainer.appendChild(li);
+
+        // Add delete button
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
 }
 
 
